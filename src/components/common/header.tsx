a6e7919612a4bda3c8d24da67a1f7898d22afc5d
배근영 <bgy09270@naver.com>
2026-04-27 13:57:30 +0900

    fix: header 컴포넌트 로고 수정(#21)

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,9 +1,12 @@
 import CircleUserRound from "@/components/ui/circle-user-round";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <div className="sticky top-0 z-999 flex h-14 w-full items-center justify-between bg-white">
-      <div className="h3-bold text-main">Peak</div>
+      <div className="h3-bold text-main">
+        <Image src={"/logo.svg"} alt="Logo" width={56} height={14} />
+      </div>
       <div>
         <CircleUserRound size={24} />
       </div>
