cb3626cf0148036d1fe93666eee5ec30e7e47503
배근영 <bgy09270@naver.com>
2026-04-29 14:38:07 +0900

    feat: 탈퇴일 날짜 포함 토스트메시지 수정 (#46)

--- src/components/login/login-error-toast.tsx
+++ src/components/login/login-error-toast.tsx
@@ -3,12 +3,17 @@
 import { useEffect } from "react";
 import { toast } from "sonner";
 
-export default function LoginErrorToast() {
+interface Props {
+  withdrawnAt?: string;
+}
+
+export default function LoginErrorToast({ withdrawnAt }: Props) {
   useEffect(() => {
-    toast.error("탈퇴 처리된 계정입니다. 90일 이후 재가입 가능합니다.", {
-      duration: 5000,
-    });
-  }, []);
+    const message = withdrawnAt
+      ? `${withdrawnAt}에 탈퇴처리된 계정입니다. 90일 이후 재가입 가능합니다.`
+      : "탈퇴처리된 계정입니다. 90일 이후 재가입 가능합니다.";
+    toast.error(message, { duration: 5000 });
+  }, [withdrawnAt]);
 
   return null;
 }
